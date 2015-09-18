bfbe436899b66d158fa0e903724e84ec8671e77b
Ben <bdspen@gmail.com>
2015-09-18 16:40:45 -0700

    add test and method to calculate pizza price, edit test and method for creating a pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-var prices = ["10", "12.50", "15"];
-var sizes = ["small", "medium", "large"];
+var pizzaOrder = [];
+var toppingSet = [];
 
+var prices = {small: "10", medium: "12.50", large: "15"};
 //toppings
 var possibleToppings = {
 pepperoni: ".50",
@@ -11,6 +12,22 @@ fetaOlive: "1.00",
 bacon: "1.00"
 };
 
+//Pizza Constructor
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+}
+
+Pizza.prototype.pizzaPrice = function(){
+  var price = this.size + this.toppings;
+  pizzaOrder.push(price);
+  return pizzaOrder[0];
+}
+
+// var toppingSet = $('input:topping-checkbox:checked').map(function () {
+//   return this.toppingSet;
+//   });
+
 // function CalculatePrice() {
 //   var pizzaPrice =
 //   for(var i = 0; i < toppings.length; i++){
@@ -18,10 +35,3 @@ bacon: "1.00"
 //     return toppingTotal;
 //   }
 // }
-
-
-//Pizza Constructor
-function Pizza(size, toppings) {
-  this.size = size;
-  this.toppingSet = [];
-}
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,20 @@
 describe('Pizza', function() {
-  it('creates a new pizza object with toppings', function() {
-    var testPizza = new Pizza("small", possibleToppings[0]);
-    expect(testPizza.size).to.equal("small", ".50");
+  it('creates a pizza object', function() {
+    var testPizza = new Pizza(prices.small, possibleToppings.pepperoni);
+    expect(testPizza.size).to.equal('10');
+    expect(testPizza.toppings).to.equal('.50');
+  });
+
+  it('creates a pizza object with two toppings', function() {
+    var testPizza = new Pizza(prices.small, [possibleToppings.pepperoni, possibleToppings.bacon]);
+    expect(testPizza.size).to.equal('10');
+    expect(testPizza.toppings).to.eql(['.50', '1.00']);
+  });
+});
+
+describe('pizza Price', function() {
+  it('calculates the price of a pizza order', function() {
+    var testPizza = new Pizza(prices.small, possibleToppings.pepperoni);
+    expect(testPizza.pizzaPrice()).to.equal('10.50');
   });
 });
